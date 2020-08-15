d945346f49fb5753955d5c211527bc4c6b6ab1ab
Orlov1302 <orlov1302@mail.ru>
2020-08-15 23:28:11 +0300

    Коммит 4

--- src/main/java/ru/sbrf/work1/Main.java
+++ src/main/java/ru/sbrf/work1/Main.java
@@ -3,6 +3,6 @@ package ru.sbrf.work1;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("----4444444------");
+        System.out.println("----55555555----");
     }
 }
